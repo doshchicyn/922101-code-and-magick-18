d2b6890e3251bfdcc44653e28878ffc22c5ee601
Max <39831143+doshchicyn@users.noreply.github.com>
2019-09-18 23:53:57 +0300

    подготовка 3 задания

---
+++ js/setup.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var popup = document.querySelector('.setup');
+popup.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-iten');
+
+var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var clothesСolor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+
+var players = [
+  {
+    'name': firstNames[0],
+  }
+];
+
